20880b8cb5352c610dbcfb32a5ce39066693872f
sharifahmad <sharifahmad2061@gmail.com>
2018-04-20 00:09:20 +0500

    completes and solves all the issues in visualizing data

--- graphs.js
+++ graphs.js
@@ -5,39 +5,75 @@ const economic_data = JSON.parse(fs.readFileSync(path.join(__dirname, 'data/econ
 const environment_data = JSON.parse(fs.readFileSync(path.join(__dirname, 'data/environmental.json'), 'utf8'));
 const social_data = JSON.parse(fs.readFileSync(path.join(__dirname, 'data/social.json'), 'utf8'));
 
-let ctx = document.querySelector("#myChart").getContext('2d');
-let chart;
+let ca_pa = document.querySelector("#ca-pa");
 
 let ra_btn_form = document.querySelector('#ra-btn-form');
 let option;
+
 ra_btn_form.addEventListener('change', () => {
     option = document.querySelector("input[name=graph-type]:checked").value;
     if (option == "economic") {
-        draw_economic_data(economic_data,ctx);
+        while (ca_pa.firstChild) {
+            ca_pa.removeChild(ca_pa.firstChild);
+        }
+        let canvas = document.createElement('canvas');
+        let w = document.createAttribute('width');
+        w.value = 800;
+        let h = document.createAttribute('height');
+        h.value = 500;
+        canvas.setAttributeNode(w);
+        canvas.setAttributeNode(h);
+        console.log(canvas);
+        ca_pa.appendChild(canvas);
+        let ctx = canvas.getContext('2d');
+        draw_economic_data(economic_data, ctx);
     } else if (option == "environmental") {
-        draw_environment_data(environment_data,ctx);
+        while (ca_pa.firstChild) {
+            ca_pa.removeChild(ca_pa.firstChild);
+        }
+        let canvas = document.createElement('canvas');
+        let w = document.createAttribute('width');
+        w.value = 800;
+        let h = document.createAttribute('height');
+        h.value = 500;
+        canvas.setAttributeNode(w);
+        canvas.setAttributeNode(h);
+        console.log(canvas);
+        ca_pa.appendChild(canvas);
+        let ctx = canvas.getContext('2d');
+        draw_environment_data(environment_data, ctx);
     } else {
-        draw_social_data(social_data,ctx);
+        while (ca_pa.firstChild) {
+            ca_pa.removeChild(ca_pa.firstChild);
+        }
+        let canvas = document.createElement('canvas');
+        let w = document.createAttribute('width');
+        w.value = 800;
+        let h = document.createAttribute('height');
+        h.value = 500;
+        canvas.setAttributeNode(w);
+        canvas.setAttributeNode(h);
+        console.log(canvas);
+        ca_pa.appendChild(canvas);
+        let ctx = canvas.getContext('2d');
+        draw_social_data(social_data, ctx);
     }
 });
 
-function draw_economic_data(data, ctx, chart) {
-    if(chart){
-        chart.destroy();
-    }
-    chart = new Chart(ctx, {
+function draw_economic_data(data, ctx) {
+    new Chart(ctx, {
         type: 'bar',
         data: {
             labels: ["X1", "X2", "X3", "X4", "X5", "X6", "X7", "X8", "X9", "X10", "X11", "X12", "X13", "X14"],
             datasets: [{
                 label: 'normalized KPI',
                 data: data['normalizedKPIValuesAchieved'],
-                backgroundColor: 'rgba(153, 102, 255, 0.8)',
+                backgroundColor: '#FFB74D',
                 borderWidth: 1
             }, {
                 label: 'max normalized KPI',
                 data: data['maxNormalizedKPIValuesAchieved'],
-                backgroundColor: 'rgba(75, 192, 192, 0.8)',
+                backgroundColor: '#00E676',
                 borderWidth: 1
             }]
         },
@@ -53,23 +89,20 @@ function draw_economic_data(data, ctx, chart) {
     });
 }
 
-function draw_environment_data(data, ctx, chart) {
-    if(chart){
-        chart.destroy();
-    }
-    chart = new Chart(ctx, {
+function draw_environment_data(data, ctx) {
+    new Chart(ctx, {
         type: 'bar',
         data: {
             labels: ["X15", "X16", "X17", "X18", "X18", "X19", "X20", "X21", "X22", "X23", "X24", "X25"],
             datasets: [{
                 label: 'normalized KPI',
                 data: data['normalizedKPIValuesAchieved'],
-                backgroundColor: 'rgba(153, 102, 255, 0.6)',
+                backgroundColor: '#26A69A',
                 borderWidth: 1
             }, {
                 label: 'max normalized KPI',
                 data: data['maxNormalizedKPIValuesAchieved'],
-                backgroundColor: 'rgba(75, 192, 192, 0.6)',
+                backgroundColor: '#00BCD4',
                 borderWidth: 1
             }]
         },
@@ -85,23 +118,20 @@ function draw_environment_data(data, ctx, chart) {
     });
 }
 
-function draw_social_data(data, ctx, chart) {
-    if(chart){
-        chart.destroy();
-    }
-    chart = new Chart(ctx, {
+function draw_social_data(data, ctx) {
+    new Chart(ctx, {
         type: 'bar',
         data: {
             labels: ["X26", "X27", "X28", "X29", "X30", "X31", "X32", "X33", "X34", "X35", "X36", "X37", "X38"],
             datasets: [{
                 label: 'normalized KPI',
                 data: data['normalizedKPIValuesAchieved'],
-                backgroundColor: 'rgba(153, 102, 255, 0.6)',
+                backgroundColor: '#F06292',
                 borderWidth: 1
             }, {
                 label: 'max normalized KPI',
                 data: data['maxNormalizedKPIValuesAchieved'],
-                backgroundColor: 'rgba(75, 192, 192, 0.6)',
+                backgroundColor: '#3F51B5',
                 borderWidth: 1
             }]
         },
