540f324d7da7995d2c87582d1de3c02a7449ec6e
sharifahmad <sharifahmad2061@gmail.com>
2018-04-19 12:47:35 +0500

    adds the data directory and functionality for writing homepage data to json file

--- homepage.js
+++ homepage.js
@@ -1,7 +1,27 @@
+const path = require('path');
+const fs = require('fs');
+
 mp_nxt = document.querySelector("#mp-nxt");
 form1 = document.querySelector(".needs-validation");
+
+let data = {};
+
 mp_nxt.addEventListener("click", () => {
     if (form1.checkValidity() === false) {
         form1.classList.add('was-validated');
+        return;
+    }
+    else{
+        data['name'] = document.querySelector(".form-group>input[name=name]").value;
+        data['designation'] = document.querySelector(".form-group>input[name=designation]").value;
+        data['industry'] = document.querySelector(".form-group>input[name=industry]").value;
+        data['address'] = document.querySelector(".form-group>input[name=address]").value;
+        data['contact'] = document.querySelector(".form-group>input[name=contact]").value;
+        data['capacity'] = document.querySelector("#cem-cap").value;
+        // console.log(data);
+
+        fs.writeFileSync(path.join(__dirname, 'data/user_data.json'),JSON.stringify(data,null,4));
+
+        window.location.href = "economicInd.html";
     }
 });
